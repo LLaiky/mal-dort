0548aedc9c8a5f82a30324bd5c17461e0b0f4e48
LLaiky <unconfigured@null.spigotmc.org>
2023-12-16 00:19:39 +0300

    Mal 4 Envanter Sekmesinin Adı Düzeltildi (Fix)

--- src/main/java/net/mcreator/maldort/init/MalDortModTabs.java
+++ src/main/java/net/mcreator/maldort/init/MalDortModTabs.java
@@ -4,30 +4,24 @@
  */
 package net.mcreator.maldort.init;
 
+import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 
-import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.network.chat.Component;
 import net.minecraft.core.registries.Registries;
 
 import net.mcreator.maldort.MalDortMod;
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class MalDortModTabs {
 	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MalDortMod.MODID);
+	public static final RegistryObject<CreativeModeTab> MAL_DORT_INVENTORY_TAB = REGISTRY.register("mal_dort_inventory_tab",
+			() -> CreativeModeTab.builder().title(Component.translatable("item_group.mal_dort.mal_dort_inventory_tab")).icon(() -> new ItemStack(MalDortModItems.DOWRITO.get())).displayItems((parameters, tabData) -> {
+				tabData.accept(MalDortModItems.DOWRITO.get());
+				tabData.accept(MalDortModBlocks.NIJIKA_BLOCK.get().asItem());
+				tabData.accept(MalDortModItems.DOWRITO_PICKAXE.get());
+			})
 
-	@SubscribeEvent
-	public static void buildTabContentsVanilla(BuildCreativeModeTabContentsEvent tabData) {
-
-		if (tabData.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS) {
-			tabData.accept(MalDortModBlocks.NIJIKA_BLOCK.get().asItem());
-		}
-
-		if (tabData.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
-			tabData.accept(MalDortModItems.DOWRITO_PICKAXE.get());
-		}
-	}
+					.build());
 }
