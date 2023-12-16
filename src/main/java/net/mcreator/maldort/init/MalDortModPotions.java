1f16a10a5e2cf988b130473c84add0801e3a94a8
LLaiky <unconfigured@null.spigotmc.org>
2023-12-16 10:37:46 +0300

    bir iki isim değiştirildi

--- src/main/java/net/mcreator/maldort/init/MalDortModPotions.java
+++ src/main/java/net/mcreator/maldort/init/MalDortModPotions.java
@@ -16,6 +16,6 @@ import net.mcreator.maldort.MalDortMod;
 
 public class MalDortModPotions {
 	public static final DeferredRegister<Potion> REGISTRY = DeferredRegister.create(ForgeRegistries.POTIONS, MalDortMod.MODID);
-	public static final RegistryObject<Potion> TURSU_SUYU_IKSIRI = REGISTRY.register("tursu_suyu_iksiri", () -> new Potion(new MobEffectInstance(MobEffects.BLINDNESS, 25, 2, false, true), new MobEffectInstance(MobEffects.HUNGER, 26, 2, false, true),
-			new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 20, 1, false, true), new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 3600, 0, false, true)));
+	public static final RegistryObject<Potion> TURSU_SUYU_IKSIRI = REGISTRY.register("tursu_suyu_iksiri",
+			() -> new Potion(new MobEffectInstance(MobEffects.BLINDNESS, 25, 2, false, true), new MobEffectInstance(MobEffects.HUNGER, 26, 2, false, true), new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 20, 1, false, true)));
 }
